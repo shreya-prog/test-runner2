cc24dcada42800c954fe55a23d6ee9869b9cf3c4
shreya-prog <teddushreya209@gmail.com>
2025-02-09 19:12:07 -0500

    Add new test runner workflow

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  function_name = var.function_name
+  handler       = "index.handler"
+  runtime       = "python3.9"
+  filename      = "lambda.zip"
+
+  role = aws_iam_role.lambda_role.arn
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
